ce271999825acb6c2089cfee874afd509863cdf3
Aymen Toukabri <aymen.toukabri@corilus.be>
2019-12-18 13:50:28 +0100

    AddCourse domain Model

---
+++ CleanArchitecture/CleanArchitecture.Domain/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Models
+{
+   public  class Course
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string IamgeUrl { get; set; }
+    }
+}
