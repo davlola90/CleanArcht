3dc32b0552d9906e87a059fe556c6cb5fbe6462e
Aymen Toukabri <aymen.toukabri@corilus.be>
2019-12-19 14:40:09 +0100

    API

---
+++ CleanArchitecture/CleanArchitecture.Api/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+      
+    }
+}
