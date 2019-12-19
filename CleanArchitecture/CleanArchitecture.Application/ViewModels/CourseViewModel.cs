cb459ab61f624c8247774049fa73101be43bb0e7
Aymen Toukabri <aymen.toukabri@corilus.be>
2019-12-19 10:57:39 +0100

    lll

---
+++ CleanArchitecture/CleanArchitecture.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
